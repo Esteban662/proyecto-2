8317dac967f3ac2c63ef5ad7db47264b92df74e2
esteban.acevedo <esteban.acevedo.98@hotmail.com>
2020-08-10 19:13:19 -0300

    Cambios en clase 10-08 001

--- home.js
+++ home.js
@@ -1,19 +1,20 @@
 
-let nombre = "" 
+let nombre = ""
 let email = ""
 let confirmacion=""
 
 
-if(localStorage.getItem(){
-    
-
+if(localStorage.getItem("nombre")){
+    alert("Bienvenido " + localStorage.getItem("nombre"))
+}else{
+    inicioUsuario(nombre,email)
 }
 
 function guardarEmail(email){
 
     email = prompt("Ingrese su email")
-    let arrEjemplo = email.split("")
-    if(arrEjemplo.includes("@") && arrEjemplo.includes(".")){
+    let arrEmail = email.split("")
+    if(arrEmail.includes("@") && arrEmail.includes(".")){
         localStorage.setItem("email", email)
     }else{alert("Datos invalidos")}
  
@@ -26,22 +27,30 @@ function emailValid(email){
 }
 
 
-
 function inicioUsuario(nombre, email){
-        confirmacion= confirm("Desea ingresar Nombre y email?")
-        if(confirmacion==true){ nombre = prompt("Ingrese su nombre")
-        localStorage.setItem("nombre", nombre)
+    confirmacion= confirm("Desea ingresar Nombre e email?")
+    if(confirmacion==true){ 
+        nombreValid(nombre)
         emailValid(email);}else{
             alert("Difrute la pagina!!!")
         }
-       
-}
+        
+    }
+    
+    
 
-
-inicioUsuario();
-
-
-
+    
+    function nombreValid(nombre){
+        do {
+            nombre = prompt("Ingrese su nombre")
+            nombre.trim(" ")
+            if(nombre == ""){
+                alert("Ingrese datos validos")
+            }else{return localStorage.setItem("nombre", nombre) }
+        } while (nombre == "");
+    }
+    
+    
 
 
 
