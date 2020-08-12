95b4be4908e2764f1f893648620805cf5c900245
esteban.acevedo <esteban.acevedo.98@hotmail.com>
2020-08-12 12:41:11 -0300

    Commit Saludos

--- home.js
+++ home.js
@@ -7,22 +7,67 @@ const fecha = new Date();
 const hora = fecha.getHours();
 
 if (localStorage.getItem("confirmacion")==null){
-
+  
   confirmacion = confirm("Desea ingresar Nombre e email?")
   localStorage.setItem("confirmacion", confirmacion)
     if(confirmacion==true){
-    inicioUsuario()
-  
+      inicioUsuario()
+      
     }
-  }else{
-    if(localStorage.getItem("nombre")){saludaHora()}
-    else{saludaHora2()}
+  }else{if(localStorage.getItem("nombre")){saludaHora()}}
+  
+  
+  
+  //Funciones
+  //Inicio de usuario
+  function inicioUsuario(){
+    nombreValid()
+    emailValid()
+    saludaHoraInicio()
   }
-     
+  //Guardar y validar nombre
+  function nombreValid(nombre){
+      do {
+          nombre = prompt("Ingrese su nombre")
+          
+          if(nombre.trim() == ""){
+              alert("Ingrese datos validos")
+          }else{return localStorage.setItem("nombre", nombre) }
+      } while (nombre=="" || nombre!==undefined);
+  }
+  //Guardar y validar email
+  function guardarEmail(email) {
+    email = prompt("Ingrese su email");
+    let arrEmail = email.split("");
+    if (arrEmail.includes("@") && arrEmail.includes(".")) {
+      localStorage.setItem("email", email);
+      enviarMail(mailTo);
+    } else {
+      alert("Datos invalidos");
+    }
+  }
+  
+  function emailValid(email) {
+    do {
+      guardarEmail(email);
+    } while (localStorage.getItem("email") === null);
+  }
 
-function saludaHora() {
-  if (hora >= 00 && hora <= 06) {
-    alert("Buenas madrugadas " + localStorage.getItem("nombre"));
+  //Preguntar para enviar mail
+  function enviarMail(mail) {
+    mail = confirm("Desea recibir mails con novedades?");
+    if (mail == true) {
+      alert(
+        "Estaremos enviandole las ultimas novedades a " +
+          localStorage.getItem("email")
+      );
+      localStorage.setItem("mail", mail);
+    }
+  }
+  //Saludar segun la hora del dia
+  function saludaHora() {
+    if (hora >= 00 && hora <= 06) {
+      alert("Buenas madrugadas " + localStorage.getItem("nombre"));
   }
   if (hora >= 07 && hora <= 12) {
     alert("Buen dia " + localStorage.getItem("nombre"));
@@ -35,65 +80,25 @@ function saludaHora() {
   }
 }
 
-function saludaHora2() {
+function saludaHoraInicio() {
   if (hora >= 00 && hora <= 06) {
-    alert("Buenas madrugadas");
-  }
-  if (hora >= 07 && hora <= 12) {
-    alert("Buen dia");
-  }
-  if (hora >= 13 && hora <= 18) {
-    alert("Buenas tardes");
-  }
-  if (hora >= 19 && hora <= 23) {
-    alert("Buenas noches");
-  }
+    alert("Buenas madrugadas " + localStorage.getItem("nombre") + ". Difrute la pagina!!!" );
 }
-
-function enviarMail(mail) {
-  mail = confirm("Desea recibir mails con novedades?");
-  if (mail == true) {
-    alert(
-      "Estaremos enviandole las ultimas novedades a " +
-        localStorage.getItem("email")
-    );
-    localStorage.setItem("mail", mail);
-  }
+if (hora >= 07 && hora <= 12) {
+  alert("Buen dia " + localStorage.getItem("nombre") + ". Difrute la pagina!!!");
 }
-
-function guardarEmail(email) {
-  email = prompt("Ingrese su email");
-  let arrEmail = email.split("");
-  if (arrEmail.includes("@") && arrEmail.includes(".")) {
-    localStorage.setItem("email", email);
-    enviarMail(mailTo);
-  } else {
-    alert("Datos invalidos");
-  }
+if (hora >= 13 && hora <= 18) {
+  alert("Buenas tardes " + localStorage.getItem("nombre") + ". Difrute la pagina!!!");
 }
-
-function emailValid(email) {
-  do {
-    guardarEmail(email);
-  } while (localStorage.getItem("email") === null);
+if (hora >= 19 && hora <= 23) {
+  alert("Buenas noches " + localStorage.getItem("nombre") + ". Difrute la pagina!!!");
 }
-
-function inicioUsuario(){
-    nombreValid()
-    emailValid()
-    alert("Difrute la pagina!!!")
 }
 
 
-    function nombreValid(nombre){
-        do {
-            nombre = prompt("Ingrese su nombre")
-            
-            if(nombre.trim() == ""){
-                alert("Ingrese datos validos")
-            }else{return localStorage.setItem("nombre", nombre) }
-        } while (nombre=="" || nombre!==undefined);
-    }
+
+
+
  
 
 
